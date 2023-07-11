58eb7747ab01e9e50d317d4fb96953a215971ba0
Andrey <rusefillc@gmail.com>
2023-07-11 11:09:24 -0400

    better code comment
    manual pick from https://github.com/matthiasbock/cantact-fw/

--- inc/slcan.h
+++ inc/slcan.h
@@ -1,7 +1,18 @@
 #ifndef _SLCAN_H
 #define _SLCAN_H
 
+/**
+ * @brief  Parses CAN frame and generates SLCAN message
+ * @return Number of bytes in generated SLCAN message
+ */
 int8_t slcan_parse_frame(uint8_t *buf, CAN_RxHeaderTypeDef *frame_header, uint8_t* frame_data);
+
+/**
+ * @brief  Parses SLCAN message and configures CAN peripheral accordingly or transmits CAN frame
+ * @param  buf: Pointer to SLCAN message
+ * @param  len: Number of bytes in SLCAN message
+ * @return Zero if successful, other values indicate an error
+ */
 int8_t slcan_parse_str(uint8_t *buf, uint8_t len);
 
 // maximum rx buffer len: extended CAN frame with timestamp 
