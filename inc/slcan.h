b29abe958a566845cadc815e190678e5bc1d4669
Andrey <rusefillc@gmail.com>
2023-07-11 11:06:24 -0400

    better code comment
    manual pick from https://github.com/matthiasbock/cantact-fw/

--- inc/slcan.h
+++ inc/slcan.h
@@ -4,8 +4,14 @@
 int8_t slcan_parse_frame(uint8_t *buf, CAN_RxHeaderTypeDef *frame_header, uint8_t* frame_data);
 int8_t slcan_parse_str(uint8_t *buf, uint8_t len);
 
-// maximum rx buffer len: extended CAN frame with timestamp 
-#define SLCAN_MTU 30 // (sizeof("T1111222281122334455667788EA5F\r")+1)
+/**
+ * The maximum transfer unit (MTU),
+ * i.e. the maximum number of bytes possible in one SLCAN message,
+ * is the length of an extended CAN frame with timestamp:
+ *
+ *  sizeof("T1111222281122334455667788EA5F\r")+1
+ */
+#define SLCAN_MTU 30
 
 #define SLCAN_STD_ID_LEN 3
 #define SLCAN_EXT_ID_LEN 8
